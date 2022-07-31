b1dfbbd90b585ea2305688e06570a3ca055a5d56
Leon Liman <leon.liman@uni-wuerzburg.de>
2022-07-31 18:09:13 +0200

    the search field is now hidden by default

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -2,34 +2,49 @@ import React from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
-const Banner = (props) => {
-  return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span id="get-part">A place to get </span>
-          <input
-            type="text"
-            id="search-box"
-            placeholder="What is it that you truly desire?"
-            onInput={(event) => {
-              let searchTerm = event.target.value;
-              if (searchTerm.length <= 2) {
-                searchTerm = undefined;
-              }
-              props.onEnterSearchTerm(
-                searchTerm,
-                (page) => agent.Items.all(searchTerm, page),
-                agent.Items.all(searchTerm)
-              );
-            }}
-          />
-          <span> the cool stuff.</span>
+class Banner extends React.Component {
+  state = {
+    hideSearchField: true,
+  };
+  render() {
+    return (
+      <div className="banner text-white">
+        <div className="container p-4 text-center">
+          <img src={logo} alt="banner" />
+          <div>
+            <span>A place to</span>
+            <span
+              id="get-part"
+              onClick={(_) => {
+                this.setState({ hideSearchField: false });
+              }}
+            >
+              {" "}
+              get{" "}
+            </span>
+            <input
+              hidden={this.state.hideSearchField}
+              type="text"
+              id="search-box"
+              placeholder="What is it that you truly desire?"
+              onInput={(event) => {
+                let searchTerm = event.target.value;
+                if (searchTerm.length <= 2) {
+                  searchTerm = undefined;
+                }
+                this.props.onEnterSearchTerm(
+                  searchTerm,
+                  (page) => agent.Items.all(searchTerm, page),
+                  agent.Items.all(searchTerm)
+                );
+              }}
+            />
+            <span> the cool stuff.</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Banner;
